0d97eb165904af13afcfafdb05a1acd8d26fd1e2
Balaji Venkatraman <balaji@moonraft.com>
2024-05-29 13:16:18 +0530

    add basic route

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import "./App.css";
-import { ProductPage } from "./pages/ProductPage";
+import './App.css';
+import { ProductPage } from './pages/ProductPage';
+import Router from './routes/Router';
 
 function App() {
   return (
     <>
-      <ProductPage />
+      {/* <ProductPage /> */}
+      <Router />
     </>
   );
 }
--- src/routes/routeConfig.tsx
+++ src/routes/routeConfig.tsx
@@ -4,6 +4,15 @@ const getRouteConfig = (): Array<RouteObject> => {
   return [
     {
       index: true,
+      element: <p>Hai</p>,
+    },
+    {
+      path: '/ar',
+      element: <p>AR</p>,
+    },
+    {
+      path: '/recommendation',
+      element: <p>Recommendation</p>,
     },
   ];
 };
