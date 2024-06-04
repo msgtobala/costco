f2cd0a384b207293ce72484cd7f80f30a79d1a3a
Balaji Venkatraman <balaji@moonraft.com>
2024-06-04 16:41:14 +0530

    ar code

--- src/components/three/ARCanvas/ARCanvas.tsx
+++ src/components/three/ARCanvas/ARCanvas.tsx
@@ -4,14 +4,14 @@ import { ARButton, XR } from '@react-three/xr';
 import { Canvas } from '@react-three/fiber';
 
 import ARScene from 'src/components/three/ARCanvas/ARScene';
-// import { useAppContext } from 'src/context/AppProvider';
-// import BottomPanelAR from 'src/components/ui/common/BottomPanelAR/BottomPanelAR';
+import { useAppContext } from 'src/context/AppProvider';
+import BottomPanelAR from 'src/components/ui/common/BottomPanelAR/BottomPanelAR';
 // import BottomPanel from 'src/components/ui/common/BottomPanel/BottomPanel';
 
 const ARCanvas: React.FC = (): JSX.Element => {
   const overlayContent = useRef<HTMLDivElement>(null);
   const [content, setContent] = useState<HTMLElement | null>(null);
-  // const { arMode } = useAppContext();
+  const { arMode } = useAppContext();
 
   useEffect(() => {
     if (overlayContent.current) {
@@ -29,7 +29,6 @@ const ARCanvas: React.FC = (): JSX.Element => {
             root: content as HTMLElement,
           },
         }}
-        // onClick={enableARMode}
         className="ar-button"
       />
       <div style={{ position: 'relative', width: '100%', height: '100vh' }}>
@@ -39,9 +38,9 @@ const ARCanvas: React.FC = (): JSX.Element => {
           </XR>
         </Canvas>
       </div>
-      {/* <div style={{ visibility: arMode ? 'visible' : 'hidden' }}>
+      <div style={{ visibility: arMode ? 'visible' : 'hidden' }}>
         <BottomPanelAR overlayContentRef={overlayContent} />
-      </div> */}
+      </div>
       {/* {arMode ? <BottomPanel overlayContentRef={overlayContent} /> : <p>Hai</p>} */}
     </>
   );
