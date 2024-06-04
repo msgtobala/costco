1d20b62233eda6f9e0063f655fdb8673192314fb
Balaji Venkatraman <balaji@moonraft.com>
2024-06-04 16:29:23 +0530

    remove unwanted code

--- src/components/three/ARCanvas/ARCanvas.tsx
+++ src/components/three/ARCanvas/ARCanvas.tsx
@@ -4,14 +4,14 @@ import { ARButton, XR } from '@react-three/xr';
 import { Canvas } from '@react-three/fiber';
 
 import ARScene from 'src/components/three/ARCanvas/ARScene';
-import { useAppContext } from 'src/context/AppProvider';
-import BottomPanelAR from 'src/components/ui/common/BottomPanelAR/BottomPanelAR';
+// import { useAppContext } from 'src/context/AppProvider';
+// import BottomPanelAR from 'src/components/ui/common/BottomPanelAR/BottomPanelAR';
 // import BottomPanel from 'src/components/ui/common/BottomPanel/BottomPanel';
 
 const ARCanvas: React.FC = (): JSX.Element => {
   const overlayContent = useRef<HTMLDivElement>(null);
   const [content, setContent] = useState<HTMLElement | null>(null);
-  const { arMode } = useAppContext();
+  // const { arMode } = useAppContext();
 
   useEffect(() => {
     if (overlayContent.current) {
